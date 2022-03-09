531b64c3b768bbeda321a3299473dd524c20a428
Igor Work <koenigigor@yandex.ru>
2022-03-09 16:01:58 +0200

    can spawn

--- Source/NetworkShoter/Private/NSPlayerStart.cpp
+++ Source/NetworkShoter/Private/NSPlayerStart.cpp
@@ -3,10 +3,25 @@
 
 #include "NSPlayerStart.h"
 
+#include "Components/CapsuleComponent.h"
+
 bool ANSPlayerStart::CanSpawn(FName CommandName)
 {
+	auto Capsule = Cast<UCapsuleComponent>(GetRootComponent());
+	Capsule->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Overlap);
+	Capsule->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	
 	//get owerlaping actors
+	TArray<AActor*> OverlappingActors;
+	GetOverlappingActors(OverlappingActors, APawn::StaticClass());
+	
+	Capsule->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	
 	//must be not any pawn
-
+	if (OverlappingActors.IsValidIndex(0))
+	{
+		return false;
+	}
+	
 	return true;
 }
