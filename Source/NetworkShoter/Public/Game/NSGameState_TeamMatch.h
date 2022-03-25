7f321c6362c2040ddd85282e3ec3a436e93aac39
Igor Work <koenigigor@yandex.ru>
2022-03-25 15:52:14 +0200

    .

---
+++ Source/NetworkShoter/Public/Game/NSGameState_TeamMatch.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Game/NSGameState.h"
+#include "NSGameState_TeamMatch.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NETWORKSHOTER_API ANSGameState_TeamMatch : public ANSGameState
+{
+	GENERATED_BODY()
+	
+};
