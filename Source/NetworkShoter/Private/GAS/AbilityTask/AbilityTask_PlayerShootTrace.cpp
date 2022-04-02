9c2525de27e9a98114d372441cba142eded4a317
koenigigor <88707183+koenigigor@users.noreply.github.com>
2022-04-02 16:10:36 +0200

    minor

--- Source/NetworkShoter/Private/GAS/AbilityTask/AbilityTask_PlayerShootTrace.cpp
+++ Source/NetworkShoter/Private/GAS/AbilityTask/AbilityTask_PlayerShootTrace.cpp
@@ -30,26 +30,27 @@ void UAbilityTask_PlayerShootTrace::Activate()
 	//ECC_GameTraceChannel2 weapon channel
 	GetWorld()->LineTraceSingleByChannel(ViewHit, ViewStart, ViewEnd, ECollisionChannel::ECC_GameTraceChannel2, QueryParams);
 
-	
+	/*
 	DrawDebugLine(GetWorld(), ViewStart, ViewEnd, FColor::Red, false, 20.f, 0, 2);
 	DrawDebugPoint(GetWorld(), ViewStart, 3.f, FColor::Red, false, 20.f);
 	DrawDebugPoint(GetWorld(), ViewEnd, 3.f, FColor::Red, false, 20.f);
-	
+	*/
 	
 	//Trace from weapon
 	UNSEquipment* Equipment = GetOwnerActor()->FindComponentByClass<UNSEquipment>();
 	ensure(Equipment && Equipment->GetEquippedWeapon());
 	FVector WeaponStart = Equipment->GetEquippedWeapon()->GetRootComponent()->GetSocketLocation("Muzzle");
-	FVector WeaponEnd = ViewHit.bBlockingHit ? ViewHit.ImpactPoint : ViewHit.TraceEnd;
+	FVector WeaponEnd = ViewHit.bBlockingHit ? ViewHit.ImpactPoint + (ViewRotation.Vector()*5.f) : ViewHit.TraceEnd;
+		//(ViewRotation.Vector()*5.f) fix when weapon trace not long enough
 	FHitResult WeaponHit;
 
 	GetWorld()->LineTraceSingleByChannel(WeaponHit, WeaponStart, WeaponEnd, ECollisionChannel::ECC_GameTraceChannel2, QueryParams);
 
-	
+	/*
 	DrawDebugLine(GetWorld(), WeaponStart, WeaponEnd, FColor::Cyan, false, 20.f, 0, 2);
 	DrawDebugPoint(GetWorld(), WeaponStart, 10.f, FColor::Cyan, false, 20.f);
 	DrawDebugPoint(GetWorld(), WeaponEnd, 10.f, FColor::Green, false, 20.f);
-	
+	*/
 	
 	// Construct TargetData
 	FGameplayAbilityTargetData_SingleTargetHit* TargetData = new FGameplayAbilityTargetData_SingleTargetHit(WeaponHit);
