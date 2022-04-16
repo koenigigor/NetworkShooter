a5dfed2f9661697762fc8f8fbe8c5c77bcedf1ad
koenigigor <88707183+koenigigor@users.noreply.github.com>
2022-04-16 21:47:50 +0200

    Move team setup functional in component

---
+++ Source/NetworkShoter/Private/Game/TeamSetupManager.cpp
@@ -0,0 +1,205 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Game/TeamSetupManager.h"
+
+#include "Game/NSGameState.h"
+#include "Game/NSPlayerState.h"
+#include "Net/UnrealNetwork.h"
+
+
+
+UTeamSetupManager::UTeamSetupManager()
+{
+	PrimaryComponentTick.bCanEverTick = false; //timers need tick ???
+	
+}
+
+void UTeamSetupManager::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME_CONDITION(UTeamSetupManager, Teams, COND_InitialOnly);
+	DOREPLIFETIME_CONDITION(UTeamSetupManager, ChooseTeamDelay, COND_InitialOnly);
+}
+
+void UTeamSetupManager::ShuffleTeam(bool AddAlreadyInTeam)
+{
+	const auto CountOfTeams = Teams.Num();
+	if (CountOfTeams == 0)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("UTeamSetupManager::ShuffleTeam, Cant shaffle Teams not setup"))
+		return;
+	}
+	
+	const auto PlayersCount = GetWorld() -> GetGameState() -> PlayerArray.Num();
+	int32 PlayersPerTeam = PlayersCount / CountOfTeams;
+	if (PlayersCount % CountOfTeams != 0)
+		PlayersPerTeam++;
+	
+	TArray<ANSPlayerState*> PlayersToShuffle;
+	GetPlayersToShuffle(PlayersToShuffle, AddAlreadyInTeam);
+	
+	TMap<uint8, int32> TeamCountMap;
+	GetTeamAndCount(TeamCountMap, AddAlreadyInTeam);
+
+	//shuffle teams
+	for (auto Player : PlayersToShuffle) 
+	{
+		//GetRandom team id, try until get team without overflow
+		uint8 TeamToAdd;
+		do
+		{
+			TeamToAdd = uint8(Teams[FMath::RandRange(0, Teams.Num()-1)]);
+		} while (TeamCountMap[TeamToAdd] >= PlayersPerTeam);
+		
+		TeamCountMap[TeamToAdd]++;
+		
+		Player->SetGenericTeamId(FGenericTeamId(TeamToAdd));
+	}
+}
+
+void UTeamSetupManager::AddPlayerInTeam_Implementation(ANSPlayerState* Player, EGameTeam PreferTeam)
+{
+	ClearIfWaitingChoose(Player);
+
+	const auto CountOfTeams = Teams.Num();
+	if (CountOfTeams == 0)
+	{
+		UE_LOG(LogTemp, Display, TEXT("UTeamSetupManager::AddPlayerInTeam, Cant shaffle Teams not setup"))
+		return;
+	}
+
+	if (!Teams.Contains(PreferTeam))
+		PreferTeam = Teams[0];
+	
+	const auto PlayersCount = GetWorld()->GetGameState() -> PlayerArray.Num();
+	int32 PlayersPerTeam = PlayersCount / CountOfTeams;
+	if (PlayersCount % CountOfTeams != 0)
+		PlayersPerTeam++;
+
+	TMap<uint8, int32> TeamCountMap;
+	GetTeamAndCount(TeamCountMap, true);
+	
+	uint8 TeamToAdd = uint8(PreferTeam);
+	while (TeamCountMap[TeamToAdd] >= PlayersPerTeam) //try until get team without overflow
+	{
+		TeamToAdd = uint8(Teams[FMath::RandRange(0, Teams.Num()-1)]);
+	}
+	
+	Player->SetGenericTeamId(FGenericTeamId(TeamToAdd));
+}
+
+bool UTeamSetupManager::AddPlayerInTeam_Validate(ANSPlayerState* Player, EGameTeam PreferTeam)
+{
+	return true;
+}
+
+void UTeamSetupManager::WaitUntilPlayerChooseTeam(APlayerController* Player)
+{
+	auto PlayerState = Player->GetPlayerState<ANSPlayerState>();
+
+	if (!PlayerState)
+	{
+		UE_LOG(LogTemp, Log, TEXT("UTeamSetupManager::WaitUntilPlayerChooseTeam PlayerState is not a ANSPlayerState"))
+	}
+	
+	FTimerHandle WaitTimerHandle;
+	WaitChooseTeamTimers.Add(PlayerState, WaitTimerHandle);
+
+	GetWorld()->GetTimerManager().SetTimer(WaitTimerHandle, this, &UTeamSetupManager::OnChooseTimeEnd, ChooseTeamDelay);
+}
+
+void UTeamSetupManager::OnChooseTimeEnd()
+{
+	//get player who ends time
+	for (const auto& Waiting : WaitChooseTeamTimers)
+	{
+		float RemainingTime = GetWorld()->GetTimerManager().GetTimerRemaining(Waiting.Value);
+		if (RemainingTime <= 0)
+		{
+			//add player in random team
+			AddPlayerInTeam(Waiting.Key, Teams[FMath::RandRange(0, Teams.Num()-1)]);
+		}
+	}
+}
+
+void UTeamSetupManager::ClearIfWaitingChoose(ANSPlayerState* Player)
+{
+	if (!WaitChooseTeamTimers.Contains(Player)) return;
+
+	auto TimerHandel = WaitChooseTeamTimers.FindAndRemoveChecked(Player);
+	GetWorld()->GetTimerManager().ClearTimer(TimerHandel);
+	
+	if (auto Controller = Cast<APlayerController>(Player->GetOwner()))
+		PlayerChooseTeamAfterWait.Broadcast(Controller);
+}
+
+TArray<ANSPlayerState*> UTeamSetupManager::GetTeam(uint8 TeamIndex)
+{
+	TArray<ANSPlayerState*> Output;
+	
+	for (auto& Player : GetWorld()->GetGameState()->PlayerArray)
+	{
+		auto NSPlayer = StaticCast<ANSPlayerState*>(Player);
+		if (NSPlayer->GetGenericTeamId().GetId() == TeamIndex)
+			Output.Add(NSPlayer);
+	}
+
+	return Output;
+}
+
+void UTeamSetupManager::GetTeamAndCount(TMap<uint8, int32>& TeamCountMap, const bool CheckIfAlreadyInTeam)
+{
+	TeamCountMap.Empty();
+
+	//create keys for used teams, for protect using this map outside
+	for (auto& Team : Teams)
+	{
+		TeamCountMap.Add(uint8(Team), 0);
+	} 
+
+	if (!CheckIfAlreadyInTeam) { return; }
+	
+	auto PlayerArray = GetWorld()->GetGameState()->PlayerArray;
+	for (const auto& Player : PlayerArray)
+	{
+		auto TeamId = Cast<IGenericTeamAgentInterface>(Player)->GetGenericTeamId().GetId();
+		if (TeamCountMap.Contains(TeamId))
+		{
+			TeamCountMap[TeamId]++;
+		}
+		else
+		{
+			TeamCountMap.Add(TeamId, 1);
+		}
+	} 
+}
+
+void UTeamSetupManager::GetPlayersToShuffle(TArray<ANSPlayerState*>& PlayersToShuffle, bool AddAlreadyInTeam)
+{
+	PlayersToShuffle.Empty();
+	
+	auto PlayerArray = GetWorld()->GetGameState() -> PlayerArray;
+	
+	for (auto& Player : PlayerArray)
+	{
+		auto NSPlayer = Cast<ANSPlayerState>(Player);
+		if (AddAlreadyInTeam)
+		{
+			PlayersToShuffle.Add(NSPlayer);
+		}
+		else
+		{
+			if (!Teams.Contains(NSPlayer->GetTeamID_Verbose()))
+			{
+				PlayersToShuffle.Add(NSPlayer);
+			}
+		}
+	} 
+}
+
+
+
+
+
---
+++ Source/NetworkShoter/Public/Game/TeamSetupManager.h
@@ -0,0 +1,77 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "TeamSetupManager.generated.h"
+
+class ANSPlayerState;
+enum class EGameTeam : uint8;
+
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FChooseTeam, APlayerController*, Player);
+
+/** GameMode component, response team management */
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class NETWORKSHOTER_API UTeamSetupManager : public UActorComponent
+{
+	GENERATED_BODY()
+	
+public:	
+	UTeamSetupManager();
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	/** Shuffle all players in teams,
+	 *  must be called from game mode on start match add players in different teams
+	 *  @AddAlreadyInTeam - Add in Shuffle array players who already added in team */
+	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly)
+	void ShuffleTeam(bool AddAlreadyInTeam = false);
+
+	/** Add player in prefer team */
+	UFUNCTION(Server, Reliable, WithValidation, BlueprintCallable)
+	void AddPlayerInTeam(ANSPlayerState* Player, EGameTeam PreferTeam);
+	bool AddPlayerInTeam_Validate(ANSPlayerState* Player, EGameTeam PreferTeam);
+
+	/** GameMode call this if new player connect while match in progress
+	 *	and wait response from delegate PlayerChooseTeamAfterWait */
+	UFUNCTION(BlueprintCallable)
+	void WaitUntilPlayerChooseTeam(APlayerController* Player);
+	
+	/** Get Array members selected team */
+	UFUNCTION(BlueprintCallable)
+	TArray<ANSPlayerState*> GetTeam(uint8 TeamIndex);
+
+	/** Return map who contain map of teams and count players joined in this team
+	 *	If @CheckIfAlreadyInTeam = false return empty map with created keys for using teams*/
+	void GetTeamAndCount(TMap<uint8, int32>& TeamCountMap, const bool CheckIfAlreadyInTeam = true);
+
+protected:
+	/** Get array player who must be shuffled in separate teams
+	 *	@AddAlreadyInTeam - Add in Shuffle array players who already added in team */
+	void GetPlayersToShuffle(TArray<ANSPlayerState*>& PlayersToShuffle, bool AddAlreadyInTeam = false);
+
+	UFUNCTION()
+	void OnChooseTimeEnd();
+
+	/** if we wait for player wait choose team, clear his timer */
+	void ClearIfWaitingChoose(ANSPlayerState* Player);
+	
+public:
+	UPROPERTY(BlueprintAssignable)
+	FChooseTeam PlayerChooseTeamAfterWait;
+	
+	/** Teams who used in this match */
+	UPROPERTY(EditDefaultsOnly, Replicated, BlueprintReadOnly, Category="Setup")
+	TArray<EGameTeam> Teams;
+
+	/** Time for player choose team manually, or he be added in random team */
+	UPROPERTY(EditDefaultsOnly, Replicated, BlueprintReadOnly, Category="Setup")
+	float ChooseTeamDelay = 10;
+
+private:
+	UPROPERTY()
+	TMap<ANSPlayerState*, FTimerHandle> WaitChooseTeamTimers;
+};
